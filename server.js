42933f0b87aad32aab97ffc3a7f5d7cde312d36f
Anthony Carrillo <carrilloanthony2698@gmail.com>
2020-07-10 11:32:16 -0400

    Added routers, working on endpoints.

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 
+const projectsRouter = require("./projects/projectsRouter");
+const actionsRouter = require("./actions/actionsRouter");
+
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/projects", projectsRouter);
+server.use("api/actions", actionsRouter);
 
 server.get("/", (req, res) => {
     res.status(200).json({ API: "Running..." });
